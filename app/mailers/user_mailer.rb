08fa8785d11ac208a8ad489f9f9fc4fd9eaff10e
Nick Spicer <nickspicer93@gmail.com>
2015-02-05 19:47:20 +0000

    add password resets & email config

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,12 +3,12 @@ class UserMailer < ActionMailer::Base
 
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: "Account Activation"
   end
 
-  def password_reset
-    @greeting = "Hi"
+  def password_reset(user)
+    @user = user
 
-    mail to: "to@example.org"
+    mail to: user.email, subject: "Password Reset"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
   before_save { self.email = email.downcase }
   before_create :create_activation_digest
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 200 }, format: { with: VALID_EMAIL_REGEX }
@@ -41,7 +41,21 @@ class User < ActiveRecord::Base
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
-  
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
 
     def create_activation_digest
